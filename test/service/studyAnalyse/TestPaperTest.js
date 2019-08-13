27e1fcad17d33537929eaaf84681071e49dfa170
unknown <tianhan@citiccard.com>
2019-08-13 10:08:24 +0800

    完善TestPaperTest

---
+++ test/service/studyAnalyse/TestPaperTest.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var TestPaper = require('../../../service/studyAnalyse/TestPaper');
+
+describe('math检查试卷分析结果',async () => {
+  var paperData = require('../../data/math.json');
+  var testPaper = new TestPaper('./test/image/math.png',paperData)
+  await testPaper.init()
+  it('总共11题，识别出的题目不少于9题',function(){
+      assert(testPaper.getSubjectInfos().length >= 9);
+  });
+})
