046c96f530d984447570b8cbfae6388b38dd6f7b
tianhan <63253294@qq.com>
2019-08-27 17:18:11 +0800

    1

--- routes/SubjectPool.js
+++ routes/SubjectPool.js
@@ -16,7 +16,7 @@ router.get('/knowledgeChildren', async (ctx, next) => {
 })
 
 router.get('/subjectList', async (ctx, next) => {
-  log.debug('ctx.request.query',ctx.request)
+  log.debug('ctx.request.query',ctx.request.query)
   var finder = new SubjectFinder()
   var data = await finder.querySubjects(ctx.request.query.knowledgeId)
   ctx.body = data
