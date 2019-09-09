269fcd4f3dd3b7e0069d9b0c12539ab9fa0ae3d1
tianhan <63253294@qq.com>
2019-09-09 15:20:01 +0800

    1

--- routes/SubjectPool.js
+++ routes/SubjectPool.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 const log = require('../util/log.js').getLogger("SubjectPool");
 const KnowledgeTree = require('../service/subjectPool/KnowledgeTree');
 const SubjectFinder = require('../service/subjectPool/SubjectFinder');
+const SubjectLoader = require('../service/subjectPool/SubjectLoader');
 
 router.get('/knowledgeTree', async (ctx, next) => {
   var tree = new KnowledgeTree()
@@ -18,6 +19,12 @@ router.get('/knowledgeChildren', async (ctx, next) => {
 router.get('/subjectList', async (ctx, next) => {
   var finder = new SubjectFinder()
   var data = await finder.querySubjects(ctx.request.query.knowledgeId)
+  ctx.body = data
+})
+
+router.get('/subjectLoad', async (ctx, next) => {
+  var loader = new SubjectLoader()
+  var data = await loader.load()
   ctx.body = data
 })
 
---
+++ service/subjectPool/subjectLoader.js
@@ -0,0 +1,61 @@
+const log = require('../../util/log').getLogger("SubjectLoader");
+const elastic = require('../../util/elastic')
+const mongo = require('../../util/mongo')
+const path = require('path');
+const uuid = require('uuid');
+const graphic = require('../../util/graphic')
+
+module.exports.load = async () => {
+  log.info('开始导入数据')
+  var collection = 'XkwSubject'
+  var condition = {}
+  var limit = 20
+  var skip = 0
+  var hasData = true
+  var loadAmount = 0
+  while(hasData){
+    var subjectData = await mongo.find(collection, condition, limit, skip)
+    log.info(`查询到${subjectData.length}条数据`)
+    if(subjectData.length >= limit){
+      hasData = true
+      var dataArr = []
+      for (var index in subjectData) {
+        //log.info('组装数据',subjectData[index])
+        var id = uuid.v1()
+        delete subjectData[index]['_id']
+        var data = {
+          index: 'subject_data_test',
+          type: 'xkw_subject',
+          body: subjectData[index]
+        }
+        await elastic.create(data)
+        loadAmount++
+        // dataArr.push(data)
+
+      }
+      // await elastic.bulk(dataArr)
+      log.info(`导入${loadAmount}条数据`)
+    }else {
+      hasData = false
+      log.info('数据导入完成',subjectData[index])
+    }
+  }
+}
+
+module.exports.deleteById = async (id) => {
+  var data = {
+    index: 'subject_data',
+    type: 'xkw_subject',
+    id: id
+  }
+  await elastic.delete(data)
+}
+module.exports.deleteAll = async () => {
+  var data = {
+    index: 'subject_data',
+    type: 'xkw_subject',
+    body: {
+      query: {}
+    }}
+  await elastic.deleteByQuery(data)
+}
