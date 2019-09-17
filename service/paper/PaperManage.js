f7a58cf36b7224b258f71f744ed0272333062659
tianhan <63253294@qq.com>
2019-09-17 15:11:36 +0800

    增加答案检查,mongo update

--- service/paper/PaperManage.js
+++ service/paper/PaperManage.js
@@ -2,35 +2,45 @@ const log = require('../../util/log').getLogger("PaperManage");
 const mongo = require('../../util/mongo');
 
 class PaperManage{
-    constructor(){}
+    constructor(){
+      this.collection = 'TestPaper'
+    }
 
     async getPaperList(userId){
-      var data = await mongo.find("TestPaper",{})
+      var data = await mongo.find(this.collection,{})
       return data
     }
 
     async getPaperInfo(paperId){
-      var data = await mongo.find("TestPaper",{'_id': paperId})
+      var data = await mongo.find(this.collection,{'_id': paperId})
       return data
     }
 
     async deletePaper(paperId){
-      var data = await mongo.remove("TestPaper",{_id: paperId})
+      var data = await mongo.remove(this.collection,{_id: paperId})
       return data
     }
 
     async getSubjectInfo(paperId, subjectId){
-      var data = await mongo.find("TestPaper",{'_id': paperId})
-      if(data.length > 0){
-        var subjects = data[0].subjects
-        for (var index in subjects) {
-          if (subjects[index]['subjectId'] == subjectId) {
-            return subjects[index]
-          }
+      var data = await mongo.find(this.collection,{'_id': paperId})
+      if(data.length == 0) return {}
+
+      var subjects = data[0].subjects
+      for (var index in subjects) {
+        if (subjects[index]['subjectId'] == subjectId) {
+          return subjects[index]
         }
-      }else{
-        return {}
       }
+
+    }
+
+    async checkSubject(paperId, subjectId, answer){
+      var data = await mongo.find(this.collection,{'_id': paperId})
+      if(data.length == 0) return {}
+
+      var data = await mongo.update(this.collection,{$set:{"subjects.$[element].answer.status":answer},{arrayFilters:[{'element.subjectId': subjectId}]})
+      return data
+
     }
 
 }
