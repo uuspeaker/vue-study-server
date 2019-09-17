0b7a099c65bd1066d13d6d33c0715c347cd8ff54
tianhan <63253294@qq.com>
2019-09-17 12:47:16 +0800

    1

--- routes/subjectRoute.js
+++ routes/subjectRoute.js
@@ -2,6 +2,13 @@ const router = require('koa-router')()
 const log = require('../util/log.js').getLogger("SubjectPool");
 const SubjectManage = require('../service/paper/SubjectManage');
 
+router.get('/subjectList', async (ctx, next) => {
+  var subjectManage = new SubjectManage()
+  var paperId = ctx.request.query.paperId
+  var data = await subjectManage.getSubjectList(paperId)
+  ctx.body = data
+})
+
 router.get('/subjectInfo', async (ctx, next) => {
   var subjectManage = new SubjectManage()
   var paperId = ctx.request.query.paperId
